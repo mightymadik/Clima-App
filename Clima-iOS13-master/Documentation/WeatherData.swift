65d84a0a99fad4362d76835748eecd4894c696ff
mightymadik <74643207+mightymadik@users.noreply.github.com>
2023-03-15 00:10:20 +0600

    Add files via upload

---
+++ Clima-iOS13-master/Documentation/WeatherData.swift
@@ -0,0 +1,18 @@
+
+
+import Foundation
+
+struct WeatherData: Decodable{
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable{
+    let temp: Double
+}
+
+struct Weather: Decodable{
+    let description: String
+    let id: Int
+}
